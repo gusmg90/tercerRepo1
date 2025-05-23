ffb964a864926e3fe3a5c6b75fdad7c725dae942
Gustavo Martinez <gusmg90@gmail.com>
2025-05-23 14:23:51 -0600

    lanzamiento

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="tercerRepo1",                           # Nombre del paquete
+    version="0.1.0",                                # Versión inicial
+    packages=find_packages(),                       # Paquetes a incluir
+    description="Un paquete pip simple de saludo",  # Breve descripción
+    author="Gustavo Martinez",                         # Tu nombre
+    author_email="gusmg90@gmail.com",                 # Tu correo electrónico
+    url="https://github.com/gusmg90/tercerRepo1",     # URL del proyecto
+)
